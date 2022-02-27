e210be5d422b822a935a218b1cf94f20b03b430f
Ramrasi Murugesan <m.ramrasi@gmail.com>
2022-02-27 13:20:17 +0530

    Build Done
    Build Done

--- excelapp/views.py
+++ excelapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponseServerError, JsonResponse
+import pandas as pd
+import json
 
-# Create your views here.
+def home(request):
+    try:
+        limit = request.GET.get('limit', 10)
+        excel = pd.read_excel('Meter0001.xlsx', engine="openpyxl", index_col=0)
+        if limit == 'all':
+            output = excel.to_json(orient="records")
+        else:
+            limit = int(limit)
+            offset = int(request.GET.get('offset', 0))
+            output = excel[offset:limit].to_json(orient="records")
+
+        return JsonResponse(json.loads(output), safe=False)
+    except Exception:
+        return HttpResponseServerError
+  
